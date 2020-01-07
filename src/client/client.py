ee05492129d22c2be10acd02f602b373ce9e02d9
Matthix7 <37193404+Matthix7@users.noreply.github.com>
2020-01-07 12:54:34 +0100

    Add client files

---
+++ src/client/client.py
@@ -0,0 +1,48 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jan  7 09:31:12 2020
+
+@author: Matthieu
+SOCKETS : 
+https://openclassrooms.com/fr/courses/235344-apprenez-a-programmer-en-python/234698-le-reseau
+THREADS:
+https://openclassrooms.com/fr/courses/235344-apprenez-a-programmer-en-python/2235545-la-programmation-parallele-avec-threading
+
+"""
+
+import socket
+from time import time
+from tkinter import *
+from interface import Interface
+
+hote = "192.168.9.198"
+port = 12800
+
+connexion_avec_serveur = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+connexion_avec_serveur.connect((hote, port))
+print("Connexion établie avec le serveur sur le port {}".format(port))
+
+
+# Interface graphique
+fenetre = Tk()
+fenetre.geometry("600x500+300+100")
+interface = Interface(fenetre, connexion_avec_serveur)
+interface.mainloop()
+
+
+
+print("Fermeture de la connexion")
+connexion_avec_serveur.close()
+
+interface.destroy()
+
+
+
+
+
+
+
+
+
+
+
---
+++ src/client/interface.py
@@ -0,0 +1,76 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jan  7 10:06:41 2020
+
+@author: catam
+"""
+from tkinter import Label, Button, Frame, BOTH, Canvas
+from PIL import Image, ImageTk  
+
+class Interface(Frame):
+    
+    """Notre fenêtre principale.
+    Tous les widgets sont stockés comme attributs de cette fenêtre."""
+    
+    def __init__(self, fenetre, connexion):
+        
+        Frame.__init__(self, fenetre)
+        self.pack(fill=BOTH)        
+        
+        self.connexion = connexion
+#         Création de nos widgets
+        self.message = Label(self, text="Commande de la caméra")
+        self.message.pack(side = "top")
+        
+        self.aquisition_button = Button(self, text="Acquisition", padx=5, pady=15, 
+                                command=self.aquisition_command, bg = 'red')
+        self.aquisition_button.pack(side="bottom")
+        
+        self.load_button = Button(self, text="load", padx=5, pady=15, 
+                                command=self.chargerImage, bg = 'green')
+        self.load_button.pack(side="right")
+        
+        self.dicimg = {}
+        self.size = (320, 240)
+        self.cadre = Canvas(self,width=self.size[0],height=self.size[1],bg="black")
+        self.cadre.pack()
+        
+        
+        
+    def chargerImage(self): 
+        im=Image.open("ENSTAVENGERS.png") 
+        photo = ImageTk.PhotoImage(im) 
+        photo = photo.resize(self.size) 
+        self.dicimg['img1'] = photo        
+        item = self.cadre.create_image(self.size[0],self.size[1],image =photo) 
+        print("charger image")
+        
+    
+    def aquisition_command(self):
+        """Il y a eu un clic sur le bouton.        
+        On envoie la demande au serveur."""
+        
+        msg_a_envoyer = "1"
+        # Peut planter si vous tapez des caractères spéciaux
+        msg_a_envoyer = msg_a_envoyer.encode()
+        # On envoie le message    
+        self.connexion.send(msg_a_envoyer)
+        
+        msg_recu = self.connexion.recv(1024)
+        
+        print(msg_recu.decode()) # Là encore, peut planter s'il y a des accents
+        
+        
+        
+if __name__ == "__main__":
+    fenetre = Tk()
+    fenetre.geometry("600x500+300+100")
+    
+    interface = Interface(fenetre, None)
+    
+    interface.mainloop()
+    interface.destroy()      
+        
+#            img = ImageTk.PhotoImage(file="ENSTAVENGERS.png")
+#        label = Label(self, image=img)
+#        label.pack()
