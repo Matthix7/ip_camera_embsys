6af83bb8065dcc91155d94f26b7157a1b1ec13e4
Matthix7 <catamatth@gmail.com>
2020-01-20 14:09:35 +0100

    Added command-line option for host address.

--- src/client/client.py
+++ src/client/client.py
@@ -15,27 +15,37 @@ from time import time
 from tkinter import *
 from interface import Interface
 
-hote = "192.168.9.198"
-port = 12800
+import sys
 
-connexion_avec_serveur = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-connexion_avec_serveur.connect((hote, port))
-print("Connexion établie avec le serveur sur le port {}".format(port))
-
-
-# Interface graphique
-fenetre = Tk()
-fenetre.geometry("600x500+300+100")
-interface = Interface(fenetre, connexion_avec_serveur)
-interface.mainloop()
-
-
-
-print("Fermeture de la connexion")
-connexion_avec_serveur.close()
-
-interface.destroy()
-
+if __name__ == "__main__":
+    
+    if len(sys.argv) != 2:
+        "Usage: python3 client.py 'server_ip'"
+        
+    else:
+        hote = sys.argv[1]
+        port = 12800
+        
+        connexion_avec_serveur = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        connexion_avec_serveur.connect((hote, port))
+        print("Connexion établie avec le serveur sur le port {}".format(port))
+        
+        
+        # Interface graphique
+        fenetre = Tk()
+        fenetre.geometry("600x500+300+100")
+        interface = Interface(fenetre, connexion_avec_serveur)
+        interface.mainloop()
+        
+        
+        
+        print("Fermeture de la connexion")
+        connexion_avec_serveur.close()
+        
+        try:
+            interface.destroy()
+        except:
+            pass
 
 
 
